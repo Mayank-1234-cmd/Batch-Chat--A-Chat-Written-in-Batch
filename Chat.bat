c15341e6bdda63c1a1cd8e4c18821fc8e5cad1cc
Mayank-1234-cmd <54957445+Mayank-1234-cmd@users.noreply.github.com>
2020-01-22 21:27:22 -0500

    Update Chat.bat

--- Chat.bat
+++ Chat.bat
@@ -3,68 +3,49 @@ color 0b
 title Chat
 cls
 
-:: Well... If you want to make mods, THAT's not allowed
-:: The Mod Section on the site coming soon!
-echo Setup...
-set /p UserNamexr=Name:
-echo To view the chat, click on COMMENTS. 
-echo Setup Done.
+: Chat
+title Chatting as %username% 
+cls
+echo Type 1 to send Hi, 2 to send How are you? , 3 to send yes, 4 to send no, and 5 to send bye(and exit the chat). Type 6 to view the chat. Type 7 to empty COMMENTS.txt
+set /p ChatInputxr=Chat:
+IF %ChatInputxr% EQU 1 (
+echo %username%: Hi>>COMMENTS.txt 
+)
+IF %ChatInputxr% EQU 2 (
+echo %username%: How are you?>>COMMENTS.txt 
+goto Chat
+)
+IF %ChatInputxr% EQU 3 (
+echo %username%: yes>>COMMENTS.txt
+goto Chat
+)
+IF %ChatInputxr% EQU 4 (
+echo %username%: no>>COMMENTS.txt
+goto Chat
+)
+IF %ChatInputxr% EQU 5 (
+echo %username%: Bye!>>COMMENTS.txt
+exit
+echo You are not supposed to see this text. It means you edited Chat.bat.
+pause
+exit
+)
+IF %ChatInputxr% EQU 6 (
+echo Press any key if you are done reading.
+type COMMENTS.txt
 pause
 goto Chat
-: Chat
-title %UserNamexr%
-cls
-set /p ChatInputxr=Chat:
-echo %UserNamexr%: %ChatInputxr%>>COMMENTS.txt
+)
+IF %ChatInputxr% EQU 7 (
+break>COMMENTS.txt
 goto Chat
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+)
+goto Else
+:: Same as saying else goto Else
+:Else
+cls
+echo %username%: %ChatInputxr%>>COMMENTS.txt
+goto Chat
+echo You should not see this text. It means you edited Chat.bat
+pause
+exit
